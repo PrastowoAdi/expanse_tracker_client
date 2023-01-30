47329bfff75f91bba27fda0dd3ff5eb9a06f74ae
prastowoadi18 <prastowoadiwork@gmail.com>
2023-01-30 07:09:15 +0700

    Integrasi with api

--- src/components/List.js
+++ src/components/List.js
@@ -1,40 +1,46 @@
 import React from "react";
 import "boxicons";
-const obj = [
-  {
-    name: "Savings",
-    color: "#f9c74f",
-  },
-  {
-    name: "Invesment",
-    color: "#f9c74f",
-  },
-  {
-    name: "Expense",
-    color: "#f9c74f",
-  },
-];
+
+import { default as api } from "../store/apiSlice";
 
 export default function List() {
+  const { data, isFetching, isSuccess, isError } = api.useGetLabelsQuery();
+  const [deleteTransaction] = api.useDeleteTransactionMutation();
+  let Transactions;
+
+  const handleClick = (e) => {
+    if (!e.target.dataset.id) return 0;
+    deleteTransaction({ _id: e.target.dataset.id });
+  };
+
+  if (isFetching) {
+    Transactions = <div>Fetching</div>;
+  } else if (isSuccess) {
+    Transactions = data.map((v, i) => (
+      <Transaction key={i} category={v} handler={handleClick}></Transaction>
+    ));
+  } else if (isError) {
+    Transactions = <div>Error</div>;
+  }
+
   return (
     <div className="flex flex-col py-6 gap-3">
       <h1 className="py-4 text-xl font-bold">History</h1>
-      {obj.map((v, i) => (
-        <Transaction key={i} category={v}></Transaction>
-      ))}
+      {Transactions}
     </div>
   );
 }
 
-function Transaction({ category }) {
+function Transaction({ category, handler }) {
   if (!category) return null;
   return (
     <div
       className="item flex justify-center bg-gray-50 py-2 rounded-r"
       style={{ borderRight: `8px solid ${category.color ?? ""}` }}
     >
-      <button className="px-3">
+      <button className="px-3" onClick={handler}>
         <box-icon
+          data-id={category._id}
           color={`${category.color ?? ""}`}
           name="trash"
           size="15px"
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import expanseSlice from "./reducer";
-
+import { apiSlice } from "./apiSlice";
 export const store = configureStore({
   reducer: {
     expense: expanseSlice,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
